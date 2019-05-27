9fd9f798db207bfa2e3cca0109d89705433c1067
takewiki <hulilei@takewiki.com.cn>
2019-05-28 00:48:33 +0800

    update

---
+++ 01-fda的数据分析.R
@@ -0,0 +1,6 @@
+library(tstk);
+#'函数型数据光滑
+libraryEx('fda');
+#'函数型回归
+libraryEx('refund');
+
---
+++ 03-rr.R
@@ -0,0 +1,6 @@
+library(tstk);
+libraryEx('knitr');
+libraryEx('pagedown');
+libraryEx('blogdown');
+
+li
